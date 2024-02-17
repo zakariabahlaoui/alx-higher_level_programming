7a2c21810d8d20491c7a57a470681173ee60ae65
zakariabahlaoui <zakariabahlaoui93@gmail.com>
2024-02-17 02:12:31 +0100

    model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -6,19 +6,14 @@ class definition of a State and an instance Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
+# Define a base class for declarative class definitions
 Base = declarative_base()
 
 
 class State(Base):
-    """
-    State class:
-    inherits from Base
-    links to the MySQL table states
-    class attribute id that represents a column of an auto-generated,
-    unique integer, cant be null and is a primary key
-    class attribute name that represents a column of a string
-    with maximum 128 characters and cant be null
-    """
-    __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    """State class representation"""
+
+    __tablename__ = "states"
+
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)

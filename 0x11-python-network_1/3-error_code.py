ce8f55d1a777ee2088686eb640ea753c0fe823b7
zakariabahlaoui <zakariabahlaoui93@gmail.com>
2024-03-02 03:46:58 +0100

    fix : task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,24 +3,13 @@
 Script that takes in a URL, send a request to URL, and dispaly body
 """
 
-import urllib.request
-import urllib.error
-import sys
 
-# Function to handle HTTP errors and print the error code
-def handle_error(e):
-    print("Error code:", e.code)
+if __name__ == "__main__":
+    import sys
+    from urllib import request, error
 
-# URL to fetch
-url = sys.argv[1]
-
-# Sending a request to the URL
-try:
-    with urllib.request.urlopen(url) as response:
-        # Reading the response body and decoding it in utf-8
-        body = response.read().decode('utf-8')
-        print(body)
-
-# Handling HTTP errors
-except urllib.error.HTTPError as e:
-    handle_error(e)
+    try:
+        with request.urlopen(sys.argv[1]) as resp:
+            print(resp.read().decode('UTF-8'))
+    except error.HTTPError as er:
+        print('Error code:', er.code)
